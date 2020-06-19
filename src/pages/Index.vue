6b960ca7da4d1ec135cf6383cd89fc9a437ea790
Manuel Aguirre <aguirre.manuel7@gmail.com>
2020-06-19 03:11:08 +0200

    improved opus styles

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -7,36 +7,9 @@
 					<div id="main">
 						<div class="inner">
 							<h1>Nahuel Klahn</h1>
-							<span class="image main"><g-image src="~/assets/images/piccover.jpg" alt="" /></span>
+							<span class="image main"><g-image src="~/assets/images/piccover.jpg" alt="Retrato en lápiz"/></span>
 							<p>{{ $page.results.edges[0].summary.body }}</p>
 						</div>
-						<section class="tiles">
-							<article class="style1">
-								<span class="image">
-									<g-image src="~/assets/images/pic01.jpg" alt="" />
-								</span>
-								<a href="generic.html">
-									<h2>Magna</h2>
-									<div class="content">
-										<p>Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros et
-											feugiat.</p>
-									</div>
-								</a>
-							</article>
-							<article class="style2">
-								<span class="image">
-									<g-image src="~/assets/images/pic02.jpg" alt="" />
-								</span>
-								<a href="generic.html">
-									<h2>Lorem</h2>
-									<div class="content">
-										<p>Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros et
-											feugiat.</p>
-									</div>
-								</a>
-							</article>
-						</section>
-						
 					</div>					
 			</div>
 
---
+++ src/pages/Opus.vue
@@ -0,0 +1,39 @@
+<template>
+  <Layout><div id="main">
+    <div class="inner">     
+        <h1>Opus</h1>
+        <section class="tiles">
+          <article class="style7">
+            <span class="image">
+              <g-image src="~/assets/images/picblack.jpg" alt="" />
+            </span>
+            <a href="generic.html">
+              <h2>Blanco</h2>
+              <div class="content">
+                <p>Dulce, frío y lechoso</p>
+              </div>
+            </a>
+          </article>
+          <article class="style8">
+            <span class="image">
+              <g-image src="~/assets/images/picfog.jpg" alt="" />
+            </span>
+            <a href="generic.html">
+              <h2>Negro</h2>
+              <div class="content">
+                <p>Ónix de mis meditaciones</p>
+              </div>
+            </a>
+          </article>
+        </section>         
+    </div>
+  </div></Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'About us'
+  }
+}
+</script>
